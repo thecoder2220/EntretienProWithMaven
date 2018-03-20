8c454c44368e82db34dfc8db117e44479666c02c
CARL <fathera6@gmail.com>
2018-03-20 14:56:44 +0100

    blah blah

---
+++ src/main/java/interfaces/B.java
@@ -0,0 +1,15 @@
+package interfaces;
+
+/**
+ * Created by CARL on 11/03/2018.
+ */
+public interface B {
+    static String toto = "";
+    private static String toto () {return "";};
+
+    private static  void log(String message, String msgPrefix) {
+        // Step 1: Connect to DataStore
+        // Step 2: Log Message with Prefix and styles etc.
+        // Step 3: Close the DataStore connection
+    }
+}
